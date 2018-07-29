9561eb60207dfd947d33ee58752cff1e45fef675
Arthas Fu <arthas.fu@gmail.com>
2018-07-29 12:58:48 +0800

    add record by day

---
+++ Baby Feeding/RecordByDayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecordByDayViewController.swift
+//  Baby Feeding
+//
+//  Created by ArthasFu on 2018/7/19.
+//  Copyright © 2018年 ArthasFu. All rights reserved.
+//
+
+import UIKit
+
+class RecordByDayViewController: UIViewController {
+
+    @IBOutlet weak var record_Tableview: UITableView!
+    
+    var feeding_data_by_day
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
